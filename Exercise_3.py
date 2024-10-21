a359c174c21556f22fad39d5f3b7c706ff8be040
Prajwal <apple@Prajwals-MacBook-Pro.local>
2024-10-20 21:32:53 -0700

    Precourse_1

--- Exercise_3.py
+++ Exercise_3.py
@@ -3,30 +3,41 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
-    
+        self.data = data  
+        self.next = next 
 class SinglyLinkedList:
     def __init__(self):
-        """
-        Create a new singly-linked list.
-        Takes O(1) time.
-        """
         self.head = None
 
     def append(self, data):
-        """
-        Insert a new element at the end of the list.
-        Takes O(n) time.
-        """
+        new_node = ListNode(data)
+        if self.head is None:
+            self.head = new_node  
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
         
     def find(self, key):
-        """
-        Search for the first element with `data` matching
-        `key`. Return the element or `None` if not found.
-        Takes O(n) time.
-        """
+        current = self.head
+        while current:
+            if current.data == key:
+                return current  
+            current = current.next
+        return None  
+
         
     def remove(self, key):
-        """
-        Remove the first occurrence of `key` in the list.
-        Takes O(n) time.
-        """
+        current = self.head
+        previous = None
+        while current:
+            if current.data == key:
+                if previous:
+                    previous.next = current.next  
+                else:
+                    self.head = current.next 
+                return True  
+            previous = current
+            current = current.next
+        return False
